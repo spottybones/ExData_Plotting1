00ff13949bb31396c0539f29d1597b10d94a1fb4
Scott Burns <scott@lentigo.net>
2015-05-10 10:06:42 -0400

    Generate Plot
    Open a PNG device, output the histogram plot to it, and close the file. Also include comments decribing the purpose of the script.

--- plot1.R
+++ plot1.R
@@ -1,3 +1,8 @@
+# plot1.R
+# generate a PNG file containing a plot of a histogram showing frequency
+# of Global Active Power observations from the supplied household power consumption
+# database for 2/1/2007 - 2/2/2007
+
 # set constants for data
 DATA_URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 LOCAL_ZIP <- "./data/household_power_consumption.zip"
@@ -16,4 +21,11 @@ full_data <- read.delim(LOCAL_DATA, sep=";", na.strings=c("?"), as.is = c(1, 2))
 # subset the data for the dates we want to plot
 plot_data <- full_data[full_data$Date == '1/2/2007' | full_data$Date == '2/2/2007',]
 
-# generate the plot
+# open a PNG file to receive the plot
+png(file="plot1.png", width=504, height=504, res=96)
+
+# generate the plog and close the file
+hist(plot_data$Global_active_power, col="red", main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+dev.off()
+
